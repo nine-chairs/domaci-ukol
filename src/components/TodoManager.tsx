d9e26b63dc038c13a6cb11437e1104a0ddcee1b6
nine-chairs <havlik9@gmail.com>
2024-03-19 20:47:29 +0100

    added basic styling

--- src/components/TodoManager.tsx
+++ src/components/TodoManager.tsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import useViewModel from './viewModel';
+import './TodoManager.css'
+import add from '../icons/add.svg'
+import remove from '../icons/remove.svg'
 
 const TodoManager: React.FC = () => {
 
   const viewModel = useViewModel()
 
   return (
-    <div>
-      <h1>To-Do List</h1>
-      <input
-        type="text"
-        placeholder="Enter a task"
-        value={viewModel.state.inputText}
-        onChange={e => viewModel.handleInputChange(e.target.value)}
-      />
-      <button onClick={viewModel.handleAddTodo}>Add Task</button>
-      <select value={viewModel.state.filterOption} onChange={e => viewModel.handleFilterChange(e.target.value)}>
-        <option value="all">All Tasks</option>
-        <option value="completed">Completed Tasks</option>
-        <option value="incomplete">Incomplete Tasks</option>
-      </select>
-      <button onClick={viewModel.handleMarkAllComplete}>Mark All Complete</button>
-      <button onClick={viewModel.handleDeleteCompleted}>Delete Completed</button>
+    <div className='toDoAppWrapper'>
+      <h1>to do list</h1>
+      <div className='inputFieldContainer'>
+
+        <input
+          className='inputField'
+          type="text"
+          placeholder="Enter a task"
+          value={viewModel.state.inputText}
+          onChange={e => viewModel.handleInputChange(e.target.value)}
+        />
+        <button className='addToDoButton' onClick={viewModel.handleAddTodo}>
+          <img className='addIcon' src={add} alt={'add a new task'} />
+        </button>
+      </div>
+
+      <div className='optionsContainer'>
+        <div className='optionsContainerLeft'>
+          <select className='toDoFilter' value={viewModel.state.filterOption} onChange={e => viewModel.handleFilterChange(e.target.value)}>
+            <option value="all">All Tasks</option>
+            <option value="completed">Completed Tasks</option>
+            <option value="incomplete">Incomplete Tasks</option>
+          </select>
+          <button className='allCompletedButton' onClick={viewModel.handleMarkAllComplete}>All Complete</button>
+        </div>
+        <button className='deleteAllCompletedButton' onClick={viewModel.handleDeleteCompleted}>
+          <img className='deleteIcon' src={remove} alt={'remove all completed tasks'} />
+        </button>
+      </div>
+
+
       <p>{viewModel.countCompletedTasks()} of {viewModel.state.todos.length} tasks completed</p>
       <ul>
         {viewModel.filteredTodos.map(todo => (
